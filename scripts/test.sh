6ef89730b63abe635a4b3bdebc1670beefd33dcd
Nikita Cedrik <nikita.cedrik@gmail.com>
2019-07-04 18:19:24 +1000

    Run test:e2e without serving previously

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,9 +5,7 @@ npm run lint
 npm run test:unit
 npm run build -- --report
 
-npx serve --single dist &
-trap "kill $!" EXIT
-npm run test:e2e -- --headless --url http://localhost:5000
+npm run test:e2e -- --headless
 
 if [[ $TRAVIS_BRANCH == "develop" ]]; then
   npm run storybook:build
